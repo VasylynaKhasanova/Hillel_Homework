9ba586455856f272ab518fbc7d5dd8e2e714defa
VasylynaKhasanova <wassilissa.ws@gmail.com>
2023-03-17 15:10:30 +0200

    new homework

---
+++ src/com/homework11/LetterCalculation.java
@@ -0,0 +1,28 @@
+package com.homework11;
+
+import java.util.*;
+
+public class LetterCalculation {
+
+
+    public static void main(String[] args) {
+        String sentence = Arrays.toString(args);
+        Set<Character> delimiters = new HashSet<>(
+                Arrays.asList(' ', '.', ',', ':', ';', '?', '!', '-', '(', ')', '[', ']'));
+
+        List<Character> letters = new ArrayList<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            if (!delimiters.contains(sentence.charAt(i))) {
+                letters.add(sentence.charAt(i));
+            }
+        }
+
+        Set<Character> characters = new HashSet<>(letters);
+        for (Character character : characters) {
+            System.out.println(character + ": " + Collections.frequency(letters, character));
+        }
+    }
+
+}
+
