0141e61485f28463c0f0700760b59cb5e295899b
VasylynaKhasanova <wassilissa.ws@gmail.com>
2023-03-18 18:08:53 +0200

    new homework

--- src/com/homework11/LetterCalculation.java
+++ src/com/homework11/LetterCalculation.java
@@ -17,11 +17,21 @@ public class LetterCalculation {
             }
         }
 
-        Set<Character> characters = new HashSet<>(letters);
-        for (Character character : characters) {
-            System.out.println(character + ": " + Collections.frequency(letters, character));
+        Set<Character> calculatedLetters = new HashSet<>();
+        for (int i = 0; i < letters.size(); i++) {
+            Character letter = letters.get(i);
+            int letterCount = 0;
+            if (calculatedLetters.contains(letter)) {
+                continue;
+            }
+            for (Character character : letters) {
+                if (character.equals(letter)) {
+                    letterCount++;
+                }
+            }
+            calculatedLetters.add(letter);
+            System.out.println("Letter " + letter + " = " + letterCount);
         }
     }
-
 }
 
